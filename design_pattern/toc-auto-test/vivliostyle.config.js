a4ef4f7e3501d61a657127f175dd0b20deb92630
oldenbourglaura <laura.oldenbourg@tib.eu>
2025-11-21 11:56:38 +0100

    create automated toc test

---
+++ design_pattern/toc-auto-test/vivliostyle.config.js
@@ -0,0 +1,20 @@
+/** @type {import('@vivliostyle/cli').VivliostyleConfigSchema} */
+module.exports = {
+  title: 'Example of Table of Contents',
+  author: 'spring-raining',
+  language: 'en',
+  size: 'A4',
+  entry: [
+    './manuscript/frontmatter.html',
+    { rel: 'contents' },
+    './manuscript/01_Computing Paradigms.html',
+    './manuscript/02_Algorithm Design and Analysis.html',
+    './manuscript/03_Systems and Architecture.html',
+  ],
+  output: 'draft.pdf',
+  toc: {
+    title: 'Table of My Contents',
+    htmlPath: 'toc.html',
+    sectionDepth: 3,
+  },
+};
